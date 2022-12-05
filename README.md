d028e6ceaa881992523eff7704ed2dfa190eebe6
Tycho Luyben <tycho@e-lab.nl>
2022-12-05 13:16:24 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple NodeJS proxy 
+
+## installation
+
+- install node 17+, npm/yarn
+- run yarn
+- yarn start
+
+## how to use
+
+- curl http://localhost:3002/api/sqlite/tables
+- curl "http://localhost:3002/api/sqlite/query?q=select+Title+from+albums"
+- curl "http://localhost:3002/api/send?message=please+can+you+write+a+sorting+algorithm+in+c"
+
+
+
+# License
+
+MIT
+
+Disclaimer: for localhost experimentation only, we accept no liability.
+
+Support my open source work by <a href="https://twitter.com/luyben">following me on twitter <img src="https://storage.googleapis.com/saasify-assets/twitter-logo.svg" alt="twitter" height="24px" align="center"></a>
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const request = require('request');
+const fs = require('fs'); // add the fs module for reading the config file
+const url = require('url'); // add the url module for parsing URLs
+const dotenv = require('dotenv'); // add the dotenv module for reading environment variables
+
+// load the values of the environment variables from the .env file
+dotenv.config();
+
+// read the forwarding.json file
+const config = JSON.parse(fs.readFileSync('forwarding.json', 'utf8'));
+
+// create an express app
+const app = express();
+
+// set up a route that accepts any method and any path
+app.all('*', (req, res) => {
+  // get the original request method, path, and body
+  const method = req.method;
+  const path = req.path;
+  const body = req.body;
+
+  // get the query string parameters
+  const query = req.query;
+
+  // get the original request headers
+  const headers = req.headers;
+
+  // get the original request URL and parse it
+  const urlObj = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl);
+
+  // find a matching URL mapping in the config
+  const mapping = config.urls.find(m => urlObj.href.startsWith(m.if));
+
+  if (!mapping) {
+    // no matching URL mapping found
+    res.status(404).send('Not Found');
+    return;
+  }
+
+  // deconstruct the mapping 
+  const urlMapping = url.parse(mapping.then);
+
+  // construct the options object for the request module
+  const options = {
+    url: mapping ? mapping.then + (mapping.then.indexOf('/', 10) >= 0 ? '' : '/') + urlObj.path.substring(urlMapping.path.length) : urlObj.href, // the URL to forward the request to
+    method, // the original request method
+    headers, // the original request headers
+    qs: query, // the query string parameters
+    json: true, // set the content type to JSON
+  };
+
+  // add the request body if the original request had one
+  if (body) {
+    options.body = body;
+  }
+
+  // make the request to the other host
+  request(options, (error, response, body) => {
+    if (error) {
+      // handle the error
+      res.status(500).send({ error });
+    } else {
+      // send the response from the other host back to the original requester
+      res.send(body);
+    }
+  });
+});
+
+
+app.listen(process.env.PORT ?? 3002, () => {
+  console.log(`Server listening on port ${process.env.PORT ?? 3002}`);
+});
+
